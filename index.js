c52050bc864293c596114da1ea115fdb2e36b75e
Thang Tran <thangtv611@gmail.com>
2023-02-27 18:00:20 +0700

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+
+class BotTelegram {
+    botId;
+    channels;
+    prefix;
+    decor;
+
+    constructor(botId, channelId, serviceName, decor = true) {
+        this.prefix = serviceName;
+        this.botId = botId;
+        this.channels = [ channelId ];
+        this.decor = decor;
+    }
+
+    buildMessage(msg, level) {
+        return `${level} <pre>${msg}</pre>`;
+    }
+
+    async sendMessage(msg) {
+        try {
+            const TELEGRAM_API = `https://api.telegram.org/bot${this.botId}/sendMessage`;
+            const text = encodeURIComponent(msg);
+
+            await Promise.all(this.channels.map(channel => {
+                const url = `${TELEGRAM_API}?chat_id=${channel}&text=${text}&parse_mode=html`;
+                return axios.get(url, { muteHttpExceptions: true });
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async warning(msg) {
+        const message = this.buildMessage(msg, '🟡️');
+        await this.sendMessage(message);
+    }
+
+    async alert(msg) {
+        const message = this.buildMessage(msg, '🔴');
+        await this.sendMessage(message);
+    }
+
+    async info(msg) {
+        const message = this.buildMessage(msg, '🟢');
+        await this.sendMessage(message);
+    }
+}
+
+module.exports = {
+    BotTelegram,
+};
---
+++ test.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const {BotTelegram} = require('./index');
+
+const bot = new BotTelegram(
+    process.env.BOT_TOKEN,
+    process.env.CHANNEL_ID,
+    "my-service",
+    true,
+);
+
+(async () => {
+    await bot.info("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum");
+    // await bot.warn("Notice please!!!!");
+    // const err = new Error("Connect db error");
+    // await bot.error(`System error: ${err}`);
+})()
